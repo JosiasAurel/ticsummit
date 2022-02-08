d97ffe77a2a63a48fffe26ccea7d2ea66dd7abc6
JosiasAurel <ndjosiasaurel@gmail.com>
2022-02-08 05:23:48 +0100

    add content source

---
+++ content/index.ts
@@ -0,0 +1,7 @@
+
+const heroText: string = `TiC aims a providing Cameroonian students with opportunities for self-initiative that are led by their ability to identify, understand and address community challenges with creative and innovative business solution.
+                        Over a period of two months, students will solve community challenges while learning tech and some entrepreneurial skills`;
+
+export {
+    heroText
+};
