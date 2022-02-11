6d4226005b9141367f05f1bb4450cd330ce27ae7
JosiasAurel <ndjosiasaurel@gmail.com>
2022-02-12 00:18:25 +0100

    add clickAction to Special and focus state color

--- components/Special.tsx
+++ components/Special.tsx
@@ -5,13 +5,19 @@ import styles from "../styles/components.module.css";
 type Props = {
     number: number
     description: string
+    image?: string
+    clickAction?: Function
 }
 
-const Special: React.FC<Props> = ({ number, description }): JSX.Element => {
+const Special: React.FC<Props> = ({ number, description, clickAction, image }): JSX.Element => {
+    const [focused, setFocused] = React.useState<boolean>(false);
     return (
-        <div className={styles.specialCard}>
+        /* sets the image on the side specials to the image held by the current Special */
+        <div style={{
+            backgroundColor: focused ? "#f7cb48" : "#235ea0"
+        }} onFocus={_ => setFocused(true)} onClick={_ => clickAction(image)} className={styles.specialCard}>
             <h1>
-                {number}
+                {number >= 10 ? number : `0${number}`}
             </h1>
             <p>
                 {description}
