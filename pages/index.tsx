b7fd43e6c437910f3dd2e6c6c9e592f9f6c9daec
JosiasAurel <ndjosiasaurel@gmail.com>
2022-03-06 20:04:49 +0100

    fix: misc

--- pages/index.tsx
+++ pages/index.tsx
@@ -124,7 +124,7 @@ const HomePage: React.FC = (): JSX.Element => {
                     TiC aims a providing Cameroonian students with opportunities
                     for self-initiative that are led by their ability to
                     identify, understand and address community challenges with
-                    creative and innovative business solution. Over a period of
+                    creative and innovative business solutions. Over a period of
                     two months, students will solve community challenges while
                     learning tech and some entrepreneurial skills...
                     <Link href="/about">
